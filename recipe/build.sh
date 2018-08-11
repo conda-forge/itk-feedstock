672acaaf213d6f1a19b08318b4485ef07216aada
Matt McCormick <matt.mccormick@kitware.com>
2018-08-11 13:55:21 -0400

    Bump ITK version to 4.13.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,12 @@
 
 # install using pip from the whl files on PyPI
 
-POST=""
+POST=".post1"
 PYPI_VER="${PY_VER:0:1}${PY_VER:2:3}"
 
 for DEP_PACKAGE in core io filtering numerics registration segmentation; do
   if [ `uname` == Darwin ]; then
-      WHL_FILE=https://pypi.org/packages/cp${PYPI_VER}/i/itk-${DEP_PACKAGE}/itk_${DEP_PACKAGE}-${PKG_VERSION}${POST}-cp${PYPI_VER}-cp${PYPI_VER}m-macosx_10_6_x86_64.whl
+      WHL_FILE=https://pypi.org/packages/cp${PYPI_VER}/i/itk-${DEP_PACKAGE}/itk_${DEP_PACKAGE}-${PKG_VERSION}${POST}-cp${PYPI_VER}-cp${PYPI_VER}m-macosx_10_9_x86_64.whl
   fi
 
   if [ `uname` == Linux ]; then
